bd640a8abbf8a2dc1505f680a7ce295bc694f0fe
Ravi Altima <ravi.altima@gmail.com>
2022-07-03 19:19:31 -0400

    Initial commit

---
+++ List Tuple Sets.py
@@ -0,0 +1,17 @@
+courses = ['math','science','social','telugu','english']
+courses.sort()
+print(courses)
+courses.append('Sanskrit')
+print(courses)
+#courses.extend('Tamil')
+print(courses)
+courses.sort(reverse=True)
+print(courses)
+
+
+for items in courses:
+    print(items)
+
+
+
+
---
+++ Strings.py
@@ -0,0 +1,19 @@
+
+
+message = 'Hello World'
+print(message)
+print(len(message))
+print(message.replace('Hello', 'Ravi'))
+print(message[0:8])
+new_message = message.upper()
+print(new_message)
+
+print(help(new_message))
+
+print(type(message))
+
+
+
+print(message.upper())
+
+
