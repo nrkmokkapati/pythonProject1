0163d2f3a0263933cd5e69440aca538b6cdf2db3
Ravi Altima <ravi.altima@gmail.com>
2022-07-04 19:34:00 -0400

    return keyword learning

---
+++ func_return.py
@@ -0,0 +1,15 @@
+def return_max(x,y):
+    if x>y:
+        return x
+    elif y>x:
+        return y
+    else:
+        print('x and y are same')
+
+return_max(34,678678)
+print(return_max(34,678678))
+
+print(return_max(34,6))
+
+print('aaaa')
+print('aaaan nb')
