1dce7e50d73ed26d9f45554888e0ff4f9288bdd6
Ravi Altima <ravi.altima@gmail.com>
2022-07-04 17:16:40 -0400

    learned f strings to use along with strings

--- Strings.py
+++ Strings.py
@@ -18,3 +18,31 @@ print(message.upper())
 print(message.upper())
 
 
+print('a', end='')
+print('b', end='')
+print('c')
+
+print('a', end=' ')
+print('b', end=' ')
+print('c')
+
+#escape sequence character
+print('What\'s your name')
+print("What's your name")
+print('''GP's quote  "God is great"''')
+
+print('This is column1 \tThis is column2')
+print('This is line1 \nThis is line2')
+
+#logical line vs physical line
+i = 5
+print(i)
+
+i = 5; print(i);
+
+i = 5; print(i)
+
+i = 5
+# Error below! Notice a single space at the start of the line
+    print('Value is', i)
+print('I repeat, the value is', i)
