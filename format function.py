fe910858e19d13f06410cc84099168fa42a34ad5
Ravi Altima <ravi.altima@gmail.com>
2022-07-03 20:26:42 -0400

    learned f strings to use along with strings

--- format function.py
+++ format function.py
@@ -4,3 +4,5 @@ print('{0} was 22 years old when he came to US'.format(name))
 print('{0} came to usa to study MS when he was {1} years old'.format(name, age))
 
 
+print(f'{name} is a young man')
+print(f'{name} was {age} years old when he came to US')
